3d700f0b722282d06fe121c69dc3c52df7069ada
zoolotoui <chag.med.amine@gmail.com>
2024-01-31 21:06:27 +0100

    core:creating relationships

--- app/Models/Destinations.php
+++ app/Models/Destinations.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Destinations extends Model
 {
     use HasFactory;
     
+    public function Recits(): HasMany{
+        return $this->hasMany(Recits::class);
+    }
 }
